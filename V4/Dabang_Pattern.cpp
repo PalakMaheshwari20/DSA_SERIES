e416abad5f6bc4ef43fb1326fa54624ddac82927
PalakMaheshwari20 <87535950+PalakMaheshwari20@users.noreply.github.com>
2022-04-05 15:39:30 +0530

    Add files via upload

---
+++ V4/Dabang_Pattern.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int
+main ()
+{
+  int n;
+  cin >> n;
+  int i = 1;
+  while (i <= n)
+    {
+      int j = 1;
+      while (j <= n - i + 1)
+	{
+	  cout << j;
+	  j++;
+	}
+      int k = 1;
+      while (k < i)
+	{
+	  cout << "**";
+	  k++;
+	}
+
+
+      int l = n - i + 1;
+      while (l >= 1)
+	{
+	  cout << l;
+	  l--;
+	}
+
+      cout << endl;
+      i++;
+    }
+
+  return 0;
+}
